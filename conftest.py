d9c74bc2dfeb1f300408d6506dd5b00acacd0b25
Aleksandr Rol <rolaleksandr75@gmail.com>
2023-05-21 15:03:47 +0900

    add test_routes for pytest

---
+++ conftest.py
@@ -0,0 +1,35 @@
+import pytest
+
+from news.models import News, Comment
+
+
+@pytest.fixture
+def author(django_user_model):
+    return django_user_model.objects.create(username='Автор')
+
+
+@pytest.fixture
+def author_client(author, client):
+    client.force_login(author)
+    return client
+
+
+@pytest.fixture
+def news():
+    news = News.objects.create(title='Заголовок новости', text='Текст новости')
+    return news
+
+
+@pytest.fixture
+def comment(news, author):
+    comment = Comment.objects.create(
+        news=news,
+        author=author,
+        text='Текст комментария'
+    )
+    return comment
+
+
+@pytest.fixture
+def pk_for_news(news):
+    return news.pk,
